eb7a85a5272fb53a4d771b3c5c3c34fa4e5f3047
Lawliet-Kira <diego.reinoso@sansano.usm.cl>
2020-06-23 18:09:50 -0400

    Inicialización del spring
    Dependencias:
    - Spring web
    - Thymeleaf

---
+++ sigicam/src/main/java/sj/vienesas/sigicam/SigicamApplication.java
@@ -0,0 +1,13 @@
+package sj.vienesas.sigicam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigicamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigicamApplication.class, args);
+	}
+
+}
---
+++ sigicam/src/test/java/sj/vienesas/sigicam/SigicamApplicationTests.java
@@ -0,0 +1,13 @@
+package sj.vienesas.sigicam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigicamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
