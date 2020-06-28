e598694b7d6e3e2c3525f4008af0cf0fad3f5e4d
El Kroko <vicente.perelli@sansano.usm.cl>
2020-06-28 17:41:44 -0400

    Creacion de React
    Cree proyecto en React, instale Bootstrap y estoy probando integrar todo.

---
+++ sigicam/src/webapp/reactjs/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>
+        Bienvenio al shoreo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
